4a96a084858613dbd4c061420006324f4fe37525
tami5 <tami5@users.noreply.github.com>
2021-12-19 23:19:50 +0000

    Update generated lua [skip ci]

--- lua/telescope/_extensions/lispdocs.lua
+++ lua/telescope/_extensions/lispdocs.lua
@@ -1,16 +1,38 @@
-local _2afile_2a = "fnl/telescope/_extensions/lispdocs.fnl"
+local _0_0 = nil
+do
+  local name_0_ = "telescope.__extensions.lispdocs"
+  local module_0_ = nil
+  do
+    local x_0_ = package.loaded[name_0_]
+    if ("table" == type(x_0_)) then
+      module_0_ = x_0_
+    else
+      module_0_ = {}
+    end
+  end
+  module_0_["aniseed/module"] = name_0_
+  module_0_["aniseed/locals"] = ((module_0_)["aniseed/locals"] or {})
+  module_0_["aniseed/local-fns"] = ((module_0_)["aniseed/local-fns"] or {})
+  package.loaded[name_0_] = module_0_
+  _0_0 = module_0_
+end
+local function _1_(...)
+  local ok_3f_0_, val_0_ = nil, nil
+  local function _1_()
+    return {require("lispdocs.finder"), require("lispdocs.util")}
+  end
+  ok_3f_0_, val_0_ = pcall(_1_)
+  if ok_3f_0_ then
+    _0_0["aniseed/local-fns"] = {require = {finder = "lispdocs.finder", util = "lispdocs.util"}}
+    return val_0_
+  else
+    return print(val_0_)
+  end
+end
+local _local_0_ = _1_(...)
+local finder = _local_0_[1]
+local util = _local_0_[2]
+local _2amodule_2a = _0_0
 local _2amodule_name_2a = "telescope.__extensions.lispdocs"
-local _2amodule_2a
-do
-  package.loaded[_2amodule_name_2a] = {}
-  _2amodule_2a = package.loaded[_2amodule_name_2a]
-end
-local _2amodule_locals_2a
-do
-  _2amodule_2a["aniseed/locals"] = {}
-  _2amodule_locals_2a = (_2amodule_2a)["aniseed/locals"]
-end
-local finder, util = require("lispdocs.finder"), require("lispdocs.util")
-do end (_2amodule_locals_2a)["finder"] = finder
-_2amodule_locals_2a["util"] = util
+do local _ = ({nil, _0_0, {{}, nil, nil, nil}})[2] end
 return telescope.register_extension({exports = {lispdocs = finder.find}})
